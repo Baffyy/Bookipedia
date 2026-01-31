8e136ec72b0597fa398920f0fc5c443627fe5e60
Baffour Gyimah <baffourgyimah@Baffours-MacBook-Air.local>
2026-01-31 12:41:22 +0000

    Add server and templating

---
+++ index.js
@@ -0,0 +1,48 @@
+// Imported our npm packages
+import express from "express";
+import pg from "pg"
+import bodyParser from "body-parser";
+import axios from "axios";
+
+// created our server 
+const app = express();
+const port = 3000;
+
+// created middlewares
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+// Linked our database to express 
+const db = new pg.Client({
+    user: "postgres",
+    host: "127.0.0.1",
+    database: "bookipedia",
+    password: "1234567",
+    port: 5432
+});
+
+db.connect();
+
+// variable for database
+let books = [];
+
+app.get("/", async (req,res) => {
+    try {
+        const showcase =  await db.query("SELECT * FROM books ORDER BY id ASC");
+        books = showcase.rows;
+
+        res.render("index.ejs", {book: books})
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Database error");
+    }
+})
+
+
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+});
